9849a6090331b0cd89dae035f52ee9fc1def67f7
helenta <helenta2614@gmail.com>
2014-12-17 10:49:28 +0200

    Main method + class path

--- ICCompiler/src/IC/Compiler.java
+++ ICCompiler/src/IC/Compiler.java
@@ -25,31 +25,31 @@ public class Compiler{
     	PrettyPrinter printer;
     	Object output;
     	
-    	if (args.length == 2) 
-    	{
-    		icFileName = args[0];
-    		txtFile = new FileReader(icFileName);
-			lex = new Scanner(txtFile);
-			Parser parser = new Parser(lex);
-			symbol = parser.parse();
-			program = (Program)symbol.value;
-			txtFile.close();
-    	}
+    	icFileName = args[0];
+		txtFile = new FileReader(icFileName);
+		lex = new Scanner(txtFile);
+		Parser parser = new Parser(lex);
+		symbol = parser.parse();
+		program = (Program)symbol.value;
+		txtFile.close();
     	
-    	if (args[1].equals("-print-ast"))
-    	{
-    		printer = new PrettyPrinter(icFileName);
-    		output = printer.visit(program);
-    	    System.out.println(output);	    
-    	}
-
-    	if (args[1].equals("-dump-symtab")) 
-    	{
-			SymbolTable table = (SymbolTable) new SymbolTableBuilder().visit(program);
-			System.out.println(table);
-			TypeChecker checker = new TypeChecker(table);
-			//checker.visit(program); 
-    	}
+		if (args.length == 2)
+		{
+	    	if (args[1].equals("-print-ast"))
+	    	{
+	    		printer = new PrettyPrinter(icFileName);
+	    		output = printer.visit(program);
+	    	    System.out.println(output);	    
+	    	}
+	
+	    	if (args[1].equals("-dump-symtab")) 
+	    	{
+				SymbolTable table = (SymbolTable) new SymbolTableBuilder().visit(program);
+				System.out.println(table);
+				TypeChecker checker = new TypeChecker(table);
+				checker.visit(program); 
+	    	}
+		}
 	}
     
 }
